b03ab7b85384e469622c5e5de698b83586ab4777
Max Battcher <me@worldmaker.net>
2012-03-22 04:16:52 +0000

    Starting Debtstack in F#

---
+++ Debtstack/MonthAgo.fs
@@ -0,0 +1,16 @@
+﻿module MonthAgo
+
+open System
+
+let monthAgo (date : DateTime) =
+    let now = DateTime.Now
+    let months = (now.Year * 12 + now.Month) - (date.Year * 12 + date.Month)
+    match months with
+        | -1 -> "next month"
+        | 0 -> "this month"
+        | 1 -> "a month ago"
+        | x when x > 1 && x < 12 -> sprintf "%A months ago" x
+        | x when x >= 12 && x < 24 -> "a year ago"
+        | x when x >= 24 -> sprintf "%A years ago" (x / 12)
+        | x when x < -1 -> "the future"
+        | _ -> "elsewhen"
---
+++ Debtstack/Transaction.fs
@@ -0,0 +1,20 @@
+﻿module Transaction
+
+open System
+
+type TransactionType =
+    Debit
+    | Credit
+    | Interest
+
+type Transaction = {
+    Type: TransactionType;
+    Value: decimal;
+    mutable Paid: decimal;
+    Date: DateTime;
+    }
+    with
+        member this.Remaining with get() = match this.Type with
+            | Debit -> this.Value + this.Paid
+            | Interest -> this.Value + this.Paid
+            | _ -> decimal 0
