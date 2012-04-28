bbd559f6ae141b7c504352f19ab22af89cc5388d
Max Battcher <me@worldmaker.net>
2012-04-28 19:57:52 +0000

    Ensure interest is handled in pennies, including the "last penny"

--- Debtstack/Strategies.fs
+++ Debtstack/Strategies.fs
@@ -15,12 +15,13 @@ module Strategies =
                         | [], _, _                                     -> ()
         let rec interesting = function
                             | (tx : TransactionState) :: txs, interest -> tx.Interest interest
-                                                                          interesting (txs, interest)
-                            | [], _                                    -> ()
+                                                                          interest + interesting (txs, interest)
+                            | [], _                                    -> 0m
         let rec screen = function
                        | (tx : TransactionState) :: txs, stack -> match tx.Contract.Transaction.Type with
                                                                   | Debit    -> screen (txs, tx :: stack)
-                                                                  | Interest -> interesting (stack, tx.Contract.Transaction.Value / decimal stack.Length)
+                                                                  | Interest -> let interest = roundpenny (tx.Contract.Transaction.Value / decimal stack.Length)
+                                                                                tx.Interest (interest - interesting (stack, interest))
                                                                                 screen (txs, stack)
                                                                   | Credit   -> payback (stack, tx.Contract.Transaction.Value, tx.Contract.Transaction.Date)
                                                                                 screen (txs, stack |> List.filter (fun x -> x.Contract.PaidDate.IsNone))
@@ -33,13 +34,15 @@ module Strategies =
                         | (tx : TransactionState) :: txs, credit, date -> payback (txs, credit - (tx.Pay (roundpenny (credit / (decimal txs.Length + 1m))) date), date)
                         | [], credit, _                                -> credit
         let rec interesting = function
-                            | (tx : TransactionState) :: txs, interest, sum -> tx.Interest (interest * tx.Remaining / sum)
-                                                                               interesting (txs, interest, sum)
-                            | [], _, _                                      -> ()
+                            | (tx : TransactionState) :: txs, interest, sum -> let intamt = roundpenny (interest * tx.Remaining / sum)
+                                                                               tx.Interest intamt
+                                                                               intamt + interesting (txs, interest, sum)
+                            | [], interest, _                               -> 0m
         let rec screen = function
                        | (tx : TransactionState) :: txs, stack -> match tx.Contract.Transaction.Type with
                                                                   | Debit    -> screen (txs, tx :: stack)
-                                                                  | Interest -> interesting (stack, tx.Contract.Transaction.Value, stack |> List.sumBy (fun tx -> tx.Remaining))
+                                                                  | Interest -> let interest = tx.Contract.Transaction.Value
+                                                                                tx.Interest (interest - interesting (stack, interest, stack |> List.sumBy (fun tx -> tx.Remaining)))
                                                                                 screen (txs, stack)
                                                                   | Credit   -> let mutable credit = tx.Contract.Transaction.Value
                                                                                 while credit > 0m do
